7715d4c3117fb857f43b4a9dbeb2cf1069f1aafa
joako perez <8bussy8@gmail.com>
2026-07-14 22:50:52 -0400

    @EnableEurekaServer

--- PlataformaCurso/register-service/src/main/java/com/plataformacurso/register_service/RegisterServiceApplication.java
+++ PlataformaCurso/register-service/src/main/java/com/plataformacurso/register_service/RegisterServiceApplication.java
@@ -2,12 +2,13 @@ package com.plataformacurso.register_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer // Habilita este servicio como Eureka Server
 public class RegisterServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RegisterServiceApplication.class, args);
 	}
-
 }
